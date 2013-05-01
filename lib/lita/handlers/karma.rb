38a290a0f73936ebe443ad734cb636f926124a31
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-05-01 02:45:29 -0700

    Implement karma command.

--- lib/lita/handlers/karma.rb
+++ lib/lita/handlers/karma.rb
@@ -5,9 +5,9 @@ require "lita"
 # - Linking
 #
 # lita:handlers:karma:terms { foo(3) }
-# lita:handlers:karma:terms:foo:modified ["jimmy", "tamara"]
+# lita:handlers:karma:modified:foo ["jimmy", "tamara"]
 # lita:handlers:karma:recent:jimmy:foo "1"
-# lita:handlers:karma:term_links:foo ["bar"]
+# lita:handlers:karma:links:foo ["bar"]
 module Lita
   module Handlers
     class Karma < Handler
@@ -16,7 +16,7 @@ module Lita
       listener :increment, /([^\s]{2,})\+\+/
       listener :decrement, /([^\s]{2,})--/
       listener :check, /([^\s]{2,})~~/
-      # command :karma, "karma"
+      command :karma, "karma"
       # command :link, /([^\s]{2,})\s*\+=\s*([^\s]{2,})/
       # command :unlink, /([^\s]{2,})\s*-=\s*([^\s]{2,})/
 
@@ -36,6 +36,15 @@ module Lita
         end
       end
 
+      def karma
+        case args.first
+        when "worst"
+          list(:zrange)
+        else
+          list(:zrevrange)
+        end
+      end
+
       private
 
       def modify(delta)
@@ -47,7 +56,7 @@ module Lita
               "term once per five minutes."
           else
             score = storage.zincrby("terms", delta, term).to_i
-            storage.sadd("terms:#{term}:modified", message.user.id)
+            storage.sadd("modified:#{term}", message.user.id)
             storage.set("recent:#{message.user.id}:#{term}", 1)
             storage.expire("recent:#{message.user.id}:#{term}", RATE_LIMIT)
             say "#{term}: #{score}"
@@ -57,6 +66,19 @@ module Lita
 
       def rate_limited?(user, term)
         storage.ttl("recent:#{user.id}:#{term}") >= 0
+      end
+
+      def list(redis_command)
+        n = (args[1] || 5).to_i - 1
+
+        terms = storage.public_send(
+          redis_command, "terms", 0, n, with_scores: true
+        )
+        terms = terms.each_with_index.map do |term_with_score, index|
+          "#{index + 1}. #{term_with_score[0]} (#{term_with_score[1].to_i})"
+        end
+
+        say terms
       end
     end
 
