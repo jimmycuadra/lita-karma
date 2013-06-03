62be84111cc0478cd611cd1e994be3d755486bbc
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-06-02 22:17:41 -0700

    Use the shorthand for testing routes.

--- spec/lita/handlers/karma_spec.rb
+++ spec/lita/handlers/karma_spec.rb
@@ -1,45 +1,10 @@
 require "spec_helper"
 
 describe Lita::Handlers::Karma, lita_handler: true do
-  describe "#increment listener" do
-    it "is triggered by term++" do
-      described_class.any_instance.should_receive(:increment)
-      chat("foo++")
-    end
-  end
-
-  describe "#decrement listener" do
-    it "is triggered by term--" do
-      described_class.any_instance.should_receive(:decrement)
-      chat("foo--")
-    end
-  end
-
-  describe "#check listener" do
-    it "is triggered by term~~" do
-      described_class.any_instance.should_receive(:check)
-      chat("foo~~")
-    end
-  end
-
-  describe "#karma command" do
-    it "is triggered by karma" do
-      described_class.any_instance.should_receive(:karma)
-      chat("#{robot.name}: karma")
-    end
-  end
-
-  describe "#link command" do
-    it "is triggered by foo += bar" do
-      described_class.any_instance.should_receive(:link)
-      chat("#{robot.name}: foo += bar")
-    end
-  end
-
-  describe "#unlink command" do
-    it "is triggered by foo -= bar" do
-      described_class.any_instance.should_receive(:unlink)
-      chat("#{robot.name}: foo -= bar")
-    end
-  end
+  it { handles("foo++").with(:increment) }
+  it { handles("foo--").with(:decrement) }
+  it { handles("foo~~").with(:check) }
+  it { handles("#{robot.name}: karma").with(:karma) }
+  it { handles("#{robot.name} foo += bar").with(:link) }
+  it { handles("#{robot.name}: foo -= bar").with(:unlink) }
 end
