b2922459038ea9b195b98ab470f2e4c0a3f95d6e
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-11-06 12:30:01 -0800

    Use clearer names in Decay.

--- lib/lita/handlers/karma/decay.rb
+++ lib/lita/handlers/karma/decay.rb
@@ -7,13 +7,13 @@ module Lita::Handlers::Karma
     def call
       return unless decay_enabled?
       cutoff = Time.now.to_i - decay_interval
-      terms = redis.zrangebyscore(:actions, '-inf', cutoff).map { |json| decay_action(json) }
-      delete_decayed(terms, cutoff)
+      terms = redis.zrangebyscore(:actions, '-inf', cutoff).map { |json| decay_from_action(json) }
+      delete_old_actions_for(terms, cutoff)
     end
 
     private
 
-    def decay_action(json)
+    def decay_from_action(json)
       action = Action.from_json(json)
       redis.zincrby(:terms, -action.delta, action.term)
       redis.zincrby("modified:#{action.term}", -1, action.user_id) if action.user_id
@@ -28,8 +28,12 @@ module Lita::Handlers::Karma
       config.decay_interval
     end
 
-    def delete_decayed(terms, cutoff)
+    def delete_old_actions_for(terms, cutoff)
       redis.zremrangebyscore(:actions, '-inf', cutoff)
+      delete_zero_modifiers_for(terms)
+    end
+
+    def delete_zero_modifiers_for(terms)
       terms.each { |term| redis.zremrangebyscore("modified:#{term}", '-inf', 0) }
     end
   end
